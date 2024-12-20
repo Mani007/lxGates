f70e9ce582f09a73b6a9abe9232eba00c8dc288d
Mani007 <mohnish.a007@gmail.com>
2024-12-20 01:59:31 +0100

    flask app running successfully with python App file modified

--- backend/App.py
+++ backend/App.py
@@ -1,13 +1,14 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!!!!</h1>"
+    return "<h1>Hello, World!!!</h1>"
 
 if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(debug=True)
