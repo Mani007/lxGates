cbc4bd45f8270f8d98bf6fb252db40b6f2d85771
Mohnish <mohnish.a007@gmail.com>
2024-12-20 01:27:53 +0000

    CORS issue fixed

--- backend/App.py
+++ backend/App.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!!!</h1>"
